ea8afb7e459eaf3fbca21b98b9949fb8c45d65e2
chenyao <yaochen2@sf-express.com>
2019-05-07 18:22:52 +0800

    新建springMVC-mybatis项目

---
+++ study-project/mvc-mybatis/src/main/java/org/study/controller/IndexController.java
@@ -0,0 +1,55 @@
+package org.study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.study.dao.mapper.IndexMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chenyao
+ * @Description:
+ * @date 2018/6/23/23:44
+ */
+@Controller
+public class IndexController {
+
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @Autowired
+    private IndexMapper indexMapper;
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String indexMapper(){
+        Integer count = indexMapper.getCount();
+        return count.toString();
+    }
+    /**
+     * rest风格
+     * @param name
+     * @param age
+     * @return
+     */
+    @RequestMapping("/rest/{name}/{age}")
+    @ResponseBody
+    public Map<String, Object> rest(@PathVariable String name,
+                                    @PathVariable Integer age){
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("name",name);
+        map.put("age",age);
+
+        return map;
+    }
+}
---
+++ study-project/mvc-mybatis/src/main/java/org/study/dao/mapper/IndexMapper.java
@@ -0,0 +1,10 @@
+package org.study.dao.mapper;
+
+/**
+ * @author chenyao
+ * @date 2019/5/7 18:10
+ * @description
+ */
+public interface IndexMapper {
+    Integer getCount();
+}
