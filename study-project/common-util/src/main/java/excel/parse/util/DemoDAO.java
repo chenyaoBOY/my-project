04fb2cb62e949ef94f2899d642389c51acd9197d
chenyao <yaochen2@sf-express.com>
2020-08-03 13:37:12 +0800

    更改项目名称

---
+++ study-project/common-util/src/main/java/excel/parse/util/DemoDAO.java
@@ -0,0 +1,14 @@
+package excel.parse.util;
+
+import java.util.List;
+
+/**
+ * @author chenyao
+ * @date 2020/7/13 11:17
+ * @description
+ */
+public class DemoDAO {
+    public void save(List<DemoData> list) {
+
+    }
+}
